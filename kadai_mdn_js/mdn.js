5a841bfb5b52dca1452a022623ead23bc4d6858e
kamigakinami <kamigaki.nami@gmail.com>
2025-01-06 01:04:40 +0900

    ファイルを追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const today = new Date();
+
+const year = today.getFullYear();
+
+const mouth = today.getMonth() +1;
+
+const day = today.getDate();
+
+console.log(year + '年' + mouth + '月' + day + '日');
